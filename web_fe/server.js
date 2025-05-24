a9b88952ad245022c5ec82c0a3db3d7413aa7eff
surbhit <surbhit.thakur@gmail.com>
2025-05-24 13:50:16 +0530

    chore: use express to serve

---
+++ web_fe/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/hello', (req, res) => {
+    res.send('Hello, world!');
+});
+
+app.post('/get_moves', (req, res) => {
+    const data = req.body;
+    console.log('Received data:', data);
+    res.json({ message: 'Data received', received: data });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
